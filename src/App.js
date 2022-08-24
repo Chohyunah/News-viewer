9cf9a4986152211a5f0d5a78dbb4c485f6201f02
BOKZA <gusqnd01@naver.com>
2022-08-25 02:46:48 +0900

    category

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { useState, useCallback } from 'react';
-import NewsList from './NewsList';
 import Categories from './Categories';
-
+import NewsList from './NewsList';
 const App = () => {
-
   const [category, setCategory] = useState('all');
-  const onSelect = useCallback(category=>setCategory(category), []);
+  const onSelect = useCallback((category) => setCategory(category), []);
+
   return (
     <>
-      <Categories category={category} onSelect={onselect} />
-      <NewsList category={category}/>
+      <Categories category={category} onSelect={onSelect} />
+      <NewsList category={category} />
     </>
   );
 };
